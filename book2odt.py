44eaaa60bffa5521e8dc3ffe66ff260172570910
Michael Torrie <torriem@gmail.com>
2021-11-23 22:28:23 -0700

    text boxes generated now, but not sure where the blue color is coming from.

--- book2odt.py
+++ book2odt.py
@@ -9,6 +9,20 @@ def ns(combined_name):
     prefix,name = combined_name.split(':')
     return "{%s}%s" % (ALL_NSMAP[prefix], name)
 
+def create_frame(pageno, frameno, x,y, width, height, zindex):
+    draw_frame = Element(ns('draw:frame'))
+    draw_frame.attrib[ns('draw:name')] = 'Frame%d' % (frameno)
+    draw_frame.attrib[ns('draw:style-name')] = 'OuterFrameStyle'
+    draw_frame.attrib[ns('svg:width')] = '%dpt' % width
+    draw_frame.attrib[ns('svg:height')] = '%dpt' % height
+    draw_frame.attrib[ns('svg:x')] = '%dpt' % x
+    draw_frame.attrib[ns('svg:y')] = '%dpt' % y
+    draw_frame.attrib[ns('text:anchor-type')] = 'page'
+    draw_frame.attrib[ns('text:anchor-page-number')] = '%d' % (pageno + 1)
+    draw_frame.attrib[ns('draw:z-index')] = '%d' % (zindex + 1)
+
+    return draw_frame
+
 bookfile = sys.argv[1]
 bookpath = os.path.dirname(os.path.abspath(bookfile))
 odffile = os.path.splitext(bookfile)[0] + '.odt'
@@ -73,6 +87,19 @@ for page_style in bs.get_page_styles():
 
     bodf.styles.master_styles.xmlnode.append(sm_p)
 
+    # create a page break paragraph style that switches to this page style
+    ss = Element(ns('style:style'))
+    ss.attrib[ns('style:family')] = 'paragraph'
+    ss.attrib[ns('style:name')] = '%sbreak' % page_style['name']
+    ss.attrib[ns('style:master-page-name')] = '%s' % page_style['name']
+
+    sp_p = Element(ns('style:paragraph-properties'))
+    sp_p.attrib[ns('fo:break-before')] = 'page'
+
+    ss.append(sp_p)
+
+    bodf.content.automatic_styles.xmlnode.append(ss)
+
 # set up default page style standard
 sm_p = Element(ns('style:master-page'))
 sm_p.attrib[ns('style:name')] = 'Standard'
@@ -126,11 +153,6 @@ for ps in bs.get_paragraph_styles():
 
     bodf.content.automatic_styles.xmlnode.append(ss)
 
-    # create a break version of that style
-    ss = Element(ns('style:style'))
-    ss.attrib[ns('style:family')] = 'paragraph'
-    ss.attrib[ns('style:name')] = '%sbreak' % ps['name']
-    ss.attrib[ns('style:master-page-name')] = '%sbreak' % ps['name']
 
 
 
@@ -160,17 +182,120 @@ for ts in bs.get_span_styles():
     ss.append(st_p)
 
     bodf.content.automatic_styles.xmlnode.append(ss)
-bodf.save()
+
+# LibreOffice Default Frame style
+style_style = Element(ns('style:style'))
+style_style.attrib[ns('style:family')] = 'graphic'
+style_style.attrib[ns('style:name')] = 'Frame'
+
+graphic_properties = Element(ns('style:graphic-properties'))
+graphic_properties.attrib[ns('fo:border')] = '0.06pt solid #000000'
+graphic_properties.attrib[ns('fo:margin-bottom')] = '0.0791in'
+graphic_properties.attrib[ns('fo:margin-top')] = '0.0791in'
+graphic_properties.attrib[ns('fo:margin-left')] = '0.0591in'
+graphic_properties.attrib[ns('fo:margin-right')] = '0.0591in'
+graphic_properties.attrib[ns('fo:padding')] = '0.0591n'
+graphic_properties.attrib[ns('style:horizontal-pos')] = 'center'
+graphic_properties.attrib[ns('style:vertical-pos')] = 'top'
+graphic_properties.attrib[ns('style:vertical-rel')] = 'paragraph-content'
+graphic_properties.attrib[ns('style:horizontal-rel')] = 'paragaph-content'
+graphic_properties.attrib[ns('style:wrap')] = 'parallel'
+
+
+# Outer frame style
+style_style = Element(ns('style:style'))
+style_style.attrib[ns('style:family')] = 'graphic'
+style_style.attrib[ns('style:name')] = 'OuterFrameStyle'
+#style_style.attrib[ns('style:parent-style-name')] = 'Frame'
+
+graphic_properties = Element(ns('style:graphic-properties'))
+graphic_properties.attrib[ns('fo:border')] = 'none'
+graphic_properties.attrib[ns('fo:margin-bottom')] = '0in'
+#graphic_properties.attrib[ns('fo:margin-top')] = '0in'
+#graphic_properties.attrib[ns('fo:margin-left')] = '0in'
+#graphic_properties.attrib[ns('fo:margin-right')] = '0in'
+graphic_properties.attrib[ns('fo:padding')] = '0in'
+graphic_properties.attrib[ns('style:horizontal-pos')] = 'from-left'
+graphic_properties.attrib[ns('style:vertical-pos')] = 'from-top'
+graphic_properties.attrib[ns('style:vertical-rel')] = 'page'
+graphic_properties.attrib[ns('style:horizontal-rel')] = 'page'
+graphic_properties.attrib[ns('style:wrap')] = 'run-through'
+
+style_style.append(graphic_properties)
+bodf.content.automatic_styles.xmlnode.append(style_style)
 
 #process pages
 print ("Converting pages...")
 
+frame_no = 0
+
 for page_no, page in enumerate(bs.pages):
+    page_item_count = 0
+
     print ("Page %d... " % (page_no+1),end='')
+    print (bs.page_info[page])
 
     if page_no > 0:
         # emit a page break
+        p = Element(ns('text:p'))
 
+        if 'pagination' in bs.page_info[page] and \
+           bs.page_info[page]['pagination'] == 'START_PAGE_NUMBERS':
+            # create a paragraph style that breaks the page and sets the page number to 1
+            ss = Element(ns('style:style'))
+            ss.attrib[ns('style:family')] = 'paragraph'
+            ss.attrib[ns('style:name')] = '%sbreakresetpageno' % bs.page_info[page]['page_style']
+            ss.attrib[ns('style:master-page-name')] = '%s' % bs.page_info[page]['page_style']
+
+            sp_p = Element(ns('style:paragraph-properties'))
+            sp_p.attrib[ns('fo:break-before')] = 'page'
+            sp_p.attrib[ns('style:page-number')] = '1'
+
+            ss.append(sp_p)
+
+            bodf.content.automatic_styles.xmlnode.append(ss)
+
+            p.attrib[ns('text:style-name')] = '%sbreakresetpageno' % bs.page_info[page]['page_style']
+        else:
+            p.attrib[ns('text:style-name')] = '%sbreak' % bs.page_info[page]['page_style']
+    else:
+        p = Element(ns('text:p'))
+    bodf.body.xmlnode.append(p)
+
+    # Text boxes
+
+    for tb in bs.text_boxes[page]:
+        outer_frame = create_frame(page_no, frame_no, tb.x, tb.y, tb.width, tb.height, page_item_count)
+        bodf.body.xmlnode.insert(2,outer_frame)
+
+        dtb = Element(ns('draw:text-box'))
+        dtb.attrib[ns('fo:max-height')] = '%dpt' % tb.height
+
+        outer_frame.append(dtb)
+
+        for p in tb.paragraphs:
+            paragraph = Element(ns('text:p'))
+            if p.style:
+                paragraph.attrib[ns('text:style-name')] = p.style
+
+            for s in p.spans:
+                #TODO variables
+                span = Element(ns('text:span'))
+                if s.style:
+                    span.attrib[ns('text:style-name')] = s.style
+                span.text = s.text
+                print (s.text)
+                paragraph.append(span)
+            dtb.append(paragraph)
+
+        page_item_count +=1
+        frame_no +=1
+
+
+
+
+
+bodf.save()
 
 
 
